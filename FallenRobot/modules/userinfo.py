ed2c9c32ceef5e9b913efb04a10e0bcc2770e3f3
ZADKIEL <116737841+Moca96@users.noreply.github.com>
2023-11-16 10:00:18 -0800

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -301,10 +301,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/FallenAssociation/7"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/Network_Zadkiel/7"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/FallenAssociation/8"
+                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/Network_Zadkiel/8"
                             ),
                         ],
                     ]
